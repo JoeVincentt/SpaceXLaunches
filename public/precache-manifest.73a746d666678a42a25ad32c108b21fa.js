6a9e69e86c96437b79eea061747117d3fc089d30
JoeVincentt <eugene.bb@hotmail.com>
2018-12-27 16:06:05 -0500

    prepared for deployment

---
+++ public/precache-manifest.73a746d666678a42a25ad32c108b21fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc6bacde77368245edd37db622f98c09",
+    "url": "/static/media/logo.fc6bacde.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78c32dc45b18ad47f8e4",
+    "url": "/static/js/main.78c32dc4.chunk.js"
+  },
+  {
+    "revision": "74448b3e6e951fad4308",
+    "url": "/static/js/1.74448b3e.chunk.js"
+  },
+  {
+    "revision": "78c32dc45b18ad47f8e4",
+    "url": "/static/css/main.4aef2be0.chunk.css"
+  },
+  {
+    "revision": "25d1e57b8ed00bb5462317baac566ec4",
+    "url": "/index.html"
+  }
+];
